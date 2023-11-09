9b5448801b3ae3b675c0e91ad8c481a13f206421
Anderson1605R <114451515+Anderson1605R@users.noreply.github.com>
2023-11-09 08:38:31 -0300

    estrutura do projeto mvc do site Neuro Conexão

---
+++ src/main/java/br/com/neuro_conexao/neuro_conexao/NeuroConexaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.neuro_conexao.neuro_conexao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuroConexaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeuroConexaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/neuro_conexao/neuro_conexao/NeuroConexaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.neuro_conexao.neuro_conexao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuroConexaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
